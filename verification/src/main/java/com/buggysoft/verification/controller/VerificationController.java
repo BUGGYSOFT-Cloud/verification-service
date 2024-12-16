cce2684f4f6d36d304493a1f585a161afdda6ffb
WillMa17 <wmasillery@gmail.com>
2024-12-16 12:46:10 +0800

    OpenAPI

--- verification/src/main/java/com/buggysoft/verification/controller/VerificationController.java
+++ verification/src/main/java/com/buggysoft/verification/controller/VerificationController.java
@@ -2,8 +2,11 @@ package com.buggysoft.verification.controller;
 
 import com.buggysoft.verification.request.GoogleExchangeRequest;
 import com.buggysoft.verification.service.VerificationService;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,16 +77,101 @@ public class VerificationController {
   }
 
   @GetMapping("/auth/callback")
+  @Operation(summary = "Get Callback", description = "Acquires user associated with Google code.")
+  @ApiResponse(
+      responseCode = "202",
+      description = "Request accepted and registration process initiated",
+      content = @Content(
+          mediaType = "application/json",
+          examples = @ExampleObject(
+              value = "{ \"executionId\": \"123e4567-e89b-12d3-a456-426614174000\", \"callbackUrl\": \"/register/status/123e4567-e89b-12d3-a456-426614174000\" }"
+          )
+      ),
+      headers = {
+          @Header(name = "Location", description = "URL to check the status of the registration process", schema = @Schema(type = "string", example = "/register/status/123e4567-e89b-12d3-a456-426614174000"))
+      }
+  )
+  @ApiResponse(
+      responseCode = "500",
+      description = "Verification failed or code expired",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Failed to trigger workflow: (error message")
+      )
+  )
   public ResponseEntity<?> callback(@RequestParam String code) {
     return verificationService.getGoogleUser(code);
   }
 
   @PostMapping("/register")
+  @Operation(summary = "Register", description = "Triggers registration workflow.")
+  @ApiResponse(
+      responseCode = "202",
+      description = "Request accepted and registration process initiated",
+      content = @Content(
+          mediaType = "application/json",
+          examples = @ExampleObject(
+              value = "{ \"executionId\": \"123e4567-e89b-12d3-a456-426614174000\", \"callbackUrl\": \"/register/status/123e4567-e89b-12d3-a456-426614174000\" }"
+          )
+      ),
+      headers = {
+          @Header(name = "Location", description = "URL to check the status of the registration process", schema = @Schema(type = "string", example = "/register/status/123e4567-e89b-12d3-a456-426614174000"))
+      }
+  )
+  @ApiResponse(
+      responseCode = "500",
+      description = "Verification failed or code expired",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Failed to trigger workflow: (error message")
+      )
+  )
   public ResponseEntity<?> register(@RequestParam String email) {
     return verificationService.triggerRegister(email, restTemplate, "BUGGYSOFT");
   }
 
   @GetMapping("/register/status")
+  @Operation(summary = "Registration status", description = "Obtains registration status.")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Registration process completed successfully",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Registration successful: <result>")
+      )
+  )
+  @ApiResponse(
+      responseCode = "202",
+      description = "Registration process is still in progress",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Request is still processing.")
+      )
+  )
+  @ApiResponse(
+      responseCode = "409",
+      description = "Registration failed due to the user already existing",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "User already exists")
+      )
+  )
+  @ApiResponse(
+      responseCode = "500",
+      description = "An error occurred while retrieving workflow status",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Unable to retrieve workflow status")
+      )
+  )
+  @ApiResponse(
+      responseCode = "500",
+      description = "Registration failed due to an internal error",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Registration failed: <error_message>")
+      )
+  )
   public ResponseEntity<?> getRegisterStatus(@RequestParam String executionId) {
     try {
       return verificationService.getRegisterStatus(executionId);
@@ -94,6 +182,34 @@ public class VerificationController {
   }
 
   @PostMapping("/exchangeGoogleToken")
+  @Operation(summary = "Exchange Google Token", description = "Exchange Google token for BuggySoft JWT token.")
+  @ApiResponse(
+      responseCode = "200",
+      description = "Google token successfully validated, and a JWT token is generated.",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "<JWT Token>")
+      )
+  )
+
+  @ApiResponse(
+      responseCode = "400",
+      description = "Google token is invalid or not provided.",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Invalid google token")
+      )
+  )
+
+  @ApiResponse(
+      responseCode = "400",
+      description = "Google token cannot be null or empty.",
+      content = @Content(
+          mediaType = "text/plain",
+          examples = @ExampleObject(value = "Google token cannot be null or empty")
+      )
+  )
+
   public ResponseEntity<?> exchangeGoogleToken(@RequestBody GoogleExchangeRequest request) {
     return verificationService.exchangeGoogleToken(request.getEmail(), request.getToken());
   }
